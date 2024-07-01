f8f09312156863c3baf6eb81a951b294a2e666d4
CubeBeveled <cubebeveled@gmail.com>
2024-07-01 12:59:43 +0200

    removed stuff ig

--- api/index.js
+++ api/index.js
@@ -1,28 +1,21 @@
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
-const app = express();
+const api = express();
 
 const port = 3000;
 
-app.use(express.json());
-app.use(express.static("public"));
+api.use(express.json());
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
+api.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-app.get("/styles.css", (req, res) => res.sendFile(path.join(__dirname, "styles.css")));
-app.get("/ztx.js", (req, res) => res.sendFile(path.join(__dirname, "ztx.js")));
-
-app.get("/ztx", (req, res) => {
-  res.sendFile(path.join(__dirname, "ztx.html"));
-});
-
-app.get("/ztx/locations", async (req, res) => {
+api.get("/ztx/locations", async (req, res) => {
   const data = await axios.get("https://my.ztx.gd/api/locations")
   res.json(data.data)
 });
 
-app.listen(port, () => console.log(`Server ready on port ${port}.`));
+api.listen(port, () => console.log(`Server ready on port ${port}.`));
 
 console.log("API loaded")
-module.exports = app;
+
+module.exports = api;
