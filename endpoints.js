8ccb74fc0c72855debc2443ec95101c454e55baa
CubeBeveled <cubebeveled@gmail.com>
2024-07-01 12:20:30 +0200

    pls work

--- endpoints.js
+++ endpoints.js
@@ -1,26 +1,26 @@
 const express = require("express");
 const axios = require("axios");
 const path = require("path");
-const api = express();
+const app = express();
 
 const port = 3000;
 
-api.use(express.json());
+app.use(express.json());
 
-api.get("/", (req, res) => res.sendFile(path.join(__dirname, "ztx.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 
-api.get("/styles.css", (req, res) => res.sendFile(path.join(__dirname, "styles.css")));
-api.get("/ztx.js", (req, res) => res.sendFile(path.join(__dirname, "ztx.js")));
+app.get("/styles.css", (req, res) => res.sendFile(path.join(__dirname, "styles.css")));
+app.get("/ztx.js", (req, res) => res.sendFile(path.join(__dirname, "ztx.js")));
 
-api.get("/ztx", (req, res) => {
+app.get("/ztx", (req, res) => {
   res.sendFile(path.join(__dirname, "ztx.html"));
 });
 
-api.get("/ztx/locations", async (req, res) => {
+app.get("/ztx/locations", async (req, res) => {
   const data = await axios.get("https://my.ztx.gd/api/locations")
   res.json(data.data)
 });
 
-api.listen(port, () => console.log(`Server ready on port ${port}.`));
+app.listen(port, () => console.log(`Server ready on port ${port}.`));
 
-module.exports = api;
+module.exports = app;
