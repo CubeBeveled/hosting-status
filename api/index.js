518e3d2d6d6d5b717d5694927b5dcb7ba7cd41f9
CubeBeveled <cubebeveled@gmail.com>
2024-07-01 13:20:21 +0200

    tf

--- api/index.js
+++ api/index.js
@@ -1,26 +1,23 @@
 const express = require("express");
 const axios = require("axios");
-const path = require("path");
-const api = express();
+const app = express();
 
-const port = 3000;
+app.use(express.json());
 
-api.use(express.json());
+app.get("/", (req, res) => res.redirect("/index.html"));
 
-api.get("/", (req, res) => res.redirect("/index.html"));
-
-api.get("/ztx/locations", async (req, res) => {
+app.get("/ztx/locations", async (req, res) => {
   const data = await axios.get("https://my.ztx.gd/api/locations")
   res.json(data.data)
 });
 
-api.get("/slice/locations", async (req, res) => {
+app.get("/slice/locations", async (req, res) => {
   const data = await axios.get("https://dash.slicehosting.tech/api/locations")
   res.json(data.data)
 });
 
-api.listen(port, () => console.log(`Server ready on port ${port}.`));
+app.listen(3000, () => console.log(`Server ready on port 3000.`));
 
 console.log("API loaded")
 
-module.exports = api;
+module.exports = app;
